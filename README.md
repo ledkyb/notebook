043c45aaa9034c0326740cfb24b87bf77fe13f1d
lloan <lloanalas@outlook.com>
2019-01-26 01:33:17 -0800

    project base switched to nextJS

--- README.md
+++
@@ -1,2 +0,0 @@
-# notebook
-React blog template that uses WP as a headless CMS. 
---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+      <Link href="/index">
+      Go to home
+      </Link>
+      <p>Hello Test.js</p>
+    </div>
+)
+
+export default Index;
